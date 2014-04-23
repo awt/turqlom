aebf6639d5109bf1b4d0a885a785d3880267e25d
Deepak Mani <deepakmani@Deepaks-MacBook-Air.local>
2014-04-23 11:33:03 +0530

    Removed puts command

--- lib/turqlom/util.rb
+++ lib/turqlom/util.rb
@@ -1,7 +1,6 @@
 class Turqlom::Util
   class << self
     def write_template(in_path, out_path, &block)
-      puts "out_path is " + out_path
       template = File.new(in_path).read
       erb = ERB.new template, nil, '%'
       File.open(out_path, 'w+') do |file|
